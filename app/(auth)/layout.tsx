2abe36ec084731975cf4439f1404cf8bffe8b71b
Anh Tuan <thongocnguyen1011@gmail.com>
2025-10-14 12:37:00 +0700

    chore: update

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,34 @@
-export default async function AuthLayout({
+"use client"
+
+import { useTheme } from "next-themes"
+
+import { Particles } from "@/components/ui/particles"
+import { useMounted } from "@/hooks/use-mounted"
+
+export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const { theme, resolvedTheme } = useTheme()
+  const mounted = useMounted()
+
+  const currentTheme = mounted ? resolvedTheme || theme : "dark"
+  const isDark = currentTheme === "dark"
+
   return (
-    <main className="flex min-h-screen w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">{children}</div>
+    <main className="relative flex min-h-screen w-full items-center justify-center p-6 md:p-10 ">
+      {mounted && (
+        <Particles
+          color={isDark ? "#ffffff" : "#000000"}
+          particleCount={25000}
+          particleSize={5}
+          animate={false}
+          className={`absolute z-9 ${isDark ? "bg-black" : "bg-white"}`}
+        />
+      )}
+
+      <div className="relative z-10 w-full max-w-sm">{children}</div>
     </main>
   )
 }
