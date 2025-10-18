b04c98bb3f9b265fe669af9ec24488b2f3d26c1e
Anh Tuan <thongocnguyen1011@gmail.com>
2025-10-18 15:28:51 +0700

    chore: update

--- routes.ts
+++ routes.ts
@@ -1,7 +1,7 @@
 const authRoutes = ["/signin"]
 const signInRoute = "/signin"
 const DEFAULT_SIGNIN_REDIRECT = "/home"
-const protectedRoutes = ["/home", "statistics", "/settings"]
+const protectedRoutes = ["/home", "/statistics", "/settings"]
 const ignoredRoutes = ["/images/"]
 
 export {
