aa6a1e314277183e95aa755abc11db057d9912a3
Anh Tuan <thongocnguyen1011@gmail.com>
2025-11-02 09:02:41 +0700

    chore: update

--- i18n.d.ts
+++ i18n.d.ts
@@ -1,10 +1,10 @@
-import { type LOCALES } from "@/i18n/config"
+import { type AppLocale } from "@/i18n/config"
 
 import type messages from "./messages/vi.json"
 
 declare module "next-intl" {
   interface AppConfig {
-    Locale: (typeof LOCALES)[number]
+    Locale: AppLocale
     Messages: typeof messages
   }
 }
