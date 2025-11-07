609b0ae37d2ca395b38fc32820a15b68ddc41997
Anh Tuan <thongocnguyen1011@gmail.com>
2025-11-07 17:12:18 +0700

    chore: comment out unused index initialization code

--- lib/db.ts
+++ lib/db.ts
@@ -10,7 +10,7 @@ import {
 declare global {
   var _mongoClientPromise: Promise<MongoClient> | undefined
   var _mongoClient: MongoClient | undefined
-  var _indexesInitialized: boolean | undefined
+  // var _indexesInitialized: boolean | undefined
 }
 
 let db: Db | undefined
@@ -26,46 +26,46 @@ function getClientPromise(): Promise<MongoClient> {
   return globalThis._mongoClientPromise
 }
 
-async function initializeIndexes(db: Db) {
-  if (globalThis._indexesInitialized) {
-    return
-  }
+// async function initializeIndexes(db: Db) {
+//   if (globalThis._indexesInitialized) {
+//     return
+//   }
 
-  try {
-    const transactionsCollection = db.collection("transactions")
-    const categoriesCollection = db.collection("categories")
+//   try {
+//     const transactionsCollection = db.collection("transactions")
+//     const categoriesCollection = db.collection("categories")
 
-    await Promise.all([
-      transactionsCollection.createIndex(
-        { userId: 1, date: -1, _id: -1 },
-        { name: "userId_date__id", background: true }
-      ),
-      transactionsCollection.createIndex(
-        { userId: 1, categoryKey: 1 },
-        { name: "userId_categoryKey", background: true }
-      ),
-    ])
+//     await Promise.all([
+//       transactionsCollection.createIndex(
+//         { userId: 1, date: -1, _id: -1 },
+//         { name: "userId_date__id", background: true }
+//       ),
+//       transactionsCollection.createIndex(
+//         { userId: 1, categoryKey: 1 },
+//         { name: "userId_categoryKey", background: true }
+//       ),
+//     ])
 
-    await Promise.all([
-      categoriesCollection.createIndex(
-        { userId: 1, label: 1, type: 1 },
-        { name: "userId_label_type", background: true }
-      ),
-      categoriesCollection.createIndex(
-        { categoryKey: 1 },
-        { name: "categoryKey", unique: true, background: true }
-      ),
-      categoriesCollection.createIndex(
-        { userId: 1, _id: -1 },
-        { name: "userId__id", background: true }
-      ),
-    ])
+//     await Promise.all([
+//       categoriesCollection.createIndex(
+//         { userId: 1, label: 1, type: 1 },
+//         { name: "userId_label_type", background: true }
+//       ),
+//       categoriesCollection.createIndex(
+//         { categoryKey: 1 },
+//         { name: "categoryKey", unique: true, background: true }
+//       ),
+//       categoriesCollection.createIndex(
+//         { userId: 1, _id: -1 },
+//         { name: "userId__id", background: true }
+//       ),
+//     ])
 
-    globalThis._indexesInitialized = true
-  } catch (error) {
-    console.error("Error initializing database indexes:", error)
-  }
-}
+//     globalThis._indexesInitialized = true
+//   } catch (error) {
+//     console.error("Error initializing database indexes:", error)
+//   }
+// }
 
 export async function connect() {
   if (db) {
@@ -76,7 +76,7 @@ export async function connect() {
   db = client.db(env.DB_NAME)
   globalThis._mongoClient = client
 
-  await initializeIndexes(db)
+  // await initializeIndexes(db)
 
   return db
 }
@@ -86,7 +86,7 @@ export async function disconnect() {
     await globalThis._mongoClient.close()
     globalThis._mongoClientPromise = undefined
     globalThis._mongoClient = undefined
-    globalThis._indexesInitialized = undefined
+    // globalThis._indexesInitialized = undefined
     db = undefined
   }
 }
