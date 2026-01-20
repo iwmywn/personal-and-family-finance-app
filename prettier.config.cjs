29c2ab931ccf874923ca38b82913e6e42e3cd62d
Anh Tuan <thongocnguyen1011@gmail.com>
2026-01-20 10:50:59 +0700

    chore: update

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,4 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+/** @type {import('prettier').Config} */
 module.exports = {
   plugins: [
     "@ianvs/prettier-plugin-sort-imports",
